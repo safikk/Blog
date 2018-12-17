37214b41d8cd2cf5fce1e82b019b7f289b8fbe43
= <=>
2018-12-17 12:31:17 +0100

    passing data

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
+  posts: Array<Object> = [
+    {
+      title: "Mon premier post",
+      content: "Lorem ipsum",
+      loveIts: 0,
+      created_at: new Date()
+
+    },
+    {
+      title: "Mon deuxième post",
+      content: "Lorem ipsum",
+      loveIts: 0,
+      created_at: new Date()
+
+    },
+    {
+      title: "Mon troisième post",
+      content: "Lorem ipsum",
+      loveIts: 0,
+      created_at: new Date()
+
+    },
+
+  ];
 
   constructor() { }
 
